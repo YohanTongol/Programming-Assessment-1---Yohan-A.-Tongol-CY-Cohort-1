899ae0d80d0f9ec04487e56a8821342042289cb0
YohanTongol <yohantongo@gmail.com>
2025-10-21 01:33:49 +0400

    Add files via upload

---
+++ Exercise 2 - Yohan A. Tongol.py
@@ -0,0 +1,8 @@
+'Exercise 2'
+num1 = 8
+num2 = 10
+final = num1 + num2
+print(final)
+#This exercise uses arithmetic operators and the variable is read as
+#an integer which then gets added and displayed with the print code
+
