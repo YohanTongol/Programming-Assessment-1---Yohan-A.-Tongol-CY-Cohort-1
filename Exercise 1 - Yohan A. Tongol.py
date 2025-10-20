3f6d8b137b333f67453d56bf18300e208c2a7f9a
YohanTongol <yohantongo@gmail.com>
2025-10-21 01:32:26 +0400

    Add files via upload

---
+++ Exercise 1 - Yohan A. Tongol.py
@@ -0,0 +1,7 @@
+'Exercise 1'
+word1= "Coding"
+word2= "Is"
+word3= "Cool"
+print(word1, word2, word3)
+# word 1 2 and 3 act as a storage for the string which is then displayed
+# using the print code
