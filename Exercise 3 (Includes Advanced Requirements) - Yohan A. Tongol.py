f7a3cdafa6cdc0400757d05066fa5a57c9d8beb9
YohanTongol <yohantongo@gmail.com>
2025-10-21 01:35:18 +0400

    Add files via upload

---
+++ Exercise 3 (Includes Advanced Requirements) - Yohan A. Tongol.py
@@ -0,0 +1,14 @@
+'Exercise 3'
+biography1 = {'fname' : input("Please Input Your First name "),
+              'sname' : input("Please Input Your Second Name "),
+              'htown' : input("Please Input Your Hometown "),
+              'age'   : input("Please Input Your Age ")}
+print("First Name=", biography1 ['fname'])
+print("Second Name=",biography1 ['sname'])
+print("Hometown=",biography1 ['htown'])
+print("Age=",biography1 ['age'])
+#This code starts with creating a dictionary which then makes the keyword value
+#all keywords are on the left of the coloum or :. And all the values are on the right
+#which grabs the user input and makes it the value, it is then all printed 
+#at the end of the code by using the print function and adding the biography
+#and using indexing of the dictionary
